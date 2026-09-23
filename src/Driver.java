bd687be9865099bfb7794c1cd443858971dde09e
Brannon Davidson <bdavid03@uafs.edu>
2026-09-22 19:32:41 -0500

    ???

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,6 @@
 public class Driver {
-    public static void main(String[] args) {
-        NextGame game = new NextGame();
-        game.initialize();
-    }
+	public static void main(String[] args) {
+		NextGame game = new NextGame();
+		game.initialize();
+	}
 }
